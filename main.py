83223ff529870ef5aa4fb158d52bb3f541dd9e0e
Ziba <ziba.jabarzare@gmail.com>
2024-09-04 15:26:29 -0400

    adding initial code to main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-def add (a,b):
-    return a+b
-### this is a new comment 
+from pathlib import Path
+import shutil
+
+
+class OrganizDir:
+    def __init__(self, to_organize_dir):
+        self.to_organize_dir = to_organize_dir
+
+    def run(self):
+        print('For test')
+
+
+if __name__ == '__main__':
+    Windows_HOME = Path('/mnt/c/Users/Ziba')
+    Organizer = OrganizDir(Windows_HOME / 'Downloads')
+    Organizer.run()
+
